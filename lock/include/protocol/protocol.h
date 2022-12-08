4f3f3ea04cde83c133887816f22b825c15a692e9
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-07 22:20:43 -0300

    feat: Protocol class with private protocol created

--- lock/include/protocol/protocol.h
+++ lock/include/protocol/protocol.h
@@ -1,6 +1,25 @@
 #ifndef CRYPTO_PROTOCOL_H
 #define CRYPTO_PROTOCOL_H
 
+#include "states.h"
+
+namespace crypto::protocol
+{
+
+class Protocol
+{
+private:
+    crypto::states::State m_state;
+public:
+    Protocol(){};
+    ~Protocol(){};
+    void set_state(crypto::states::State st);
+    void state_machine();
+};
+
+
+
+} // namespace 
 
 
 #endif
--- lock/src/protocol/protocol.cpp
+++ lock/src/protocol/protocol.cpp
@@ -1,3 +1,42 @@
 #include "protocol.h"
 
+namespace crypto::protocol{
+    void Protocol::state_machine(){
+        switch (m_state)
+        {
+        case crypto::states::State::send_any_packet:
+            /* code */
+            break;
+        case crypto::states::State::encrypt_packet:
+            /* code */
+            break;
+        case crypto::states::State::decrypt_packet:
+            /* code */
+            break;
+        case crypto::states::State::wait_for_request:
+            /* code */
+            break;
+        case crypto::states::State::check_request_id:
+            /* code */
+            break;
+        case crypto::states::State::ask_for_new_id:
+            /* code */
+            break;
+        case crypto::states::State::generate_sum_chalenge:
+            /* code */
+            break;
+        case crypto::states::State::check_sum_chalenge:
+            /* code */
+            break;
+        case crypto::states::State::check_sum_chalenge_id:
+            /* code */
+            break;
+            
+        default:
+            break;
+        }
+    }
+}
+
+
 
