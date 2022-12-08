cfda57dcb304744cda89c8d4f267764ca24407ea
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-07 22:20:11 -0300

    feat: States of state machine

---
+++ lock/include/protocol/states.h
@@ -0,0 +1,23 @@
+#ifndef CRYPTO_STATES_H
+#define CRYPTO_STATES_H
+
+
+namespace crypto::states{
+
+enum class State{
+    send_any_packet=1,
+    encrypt_packet,
+    decrypt_packet,
+    wait_for_request,
+    check_request_id,
+    ask_for_new_id,
+    generate_sum_chalenge,
+    check_sum_chalenge,
+    check_sum_chalenge_id,
+};
+
+}
+
+
+
+#endif
