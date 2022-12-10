d00398bcb86c3d3d07f1fcc53132514c2a0905b3
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-10 10:56:34 -0300

    refactor: Reorganizing folders

---
+++ lock/cmake/update_git_submodules.cmake
@@ -0,0 +1,19 @@
+# DOWNLOAD ALL THE SUBMODULES
+find_package(Git QUIET)
+if(GIT_FOUND AND EXISTS "${PICO_SDK_PATH}/.git")
+# Update submodules ad needed
+  option(GIT_SUBMODULE "Check Submodule during build" ON)
+  if(GIT_SUBMODULE)
+    message(STATUS "Submodule update")
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      RESULT_VARIABLE GIT_SUBMOD_RESULT
+    )
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+      message(FATAL_ERROR "git submodule update --init --recursive \
+      failed with ${GIT_SUBMOD_RESULT}, please  checkout submodules")
+    endif()
+  endif()
+else()
+  message(STATUS "WARN -- .git folder not founded")
+endif()
