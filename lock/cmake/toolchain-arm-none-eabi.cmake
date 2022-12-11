af129ddca3cb410e8f379b1288cd94c4ea5f185e
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-10 22:53:41 -0300

    feat: Update toolchain configs
    Prefix local to install.
    Organizing C and C++ flags
    Specify linker flags

--- lock/cmake/toolchain-arm-none-eabi.cmake
+++ lock/cmake/toolchain-arm-none-eabi.cmake
@@ -1,26 +1,32 @@
 # CMake toolchain file for building ARM software libs (for RP2040 MCU)
 
 # Processor
-SET(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 # this one is important
-SET(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_NAME Generic)
 #this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_VERSION 1)
 
-# (OPTIONAL) specify some CPU flags
-SET(cpu_flags "-mcpu=cortex-m0plus -mthumb")
+set(CMAKE_STAGING_PREFIX /home/lincoln/TFG/lock)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/arm-none-eabi-gcc ${cpu_flags})
-SET(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++ ${cpu_flags})
+set(CMAKE_C_COMPILER   /usr/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++)
+set(CMAKE_MAKE_PROGRAM=/usr/bin/make)
 
-SET(CMAKE_STRIP /usr/bin/arm-none-eabi-strip)
+set(CMAKE_C_FLAGS="-mcpu=cortex-m0plus -mthumb -O2")
+set(CMAKE_CXX_FLAGS="-mcpu=cortex-m0plus -mthumb -O2")
+
+set(CMAKE_EXE_LINKER_FLAGS " --specs=nosys.specs")
+
+set(CMAKE_STRIP /usr/bin/arm-none-eabi-strip)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /usr/arm-none-eabi)
+set(CMAKE_FIND_ROOT_PATH  /usr/arm-none-eabi)
 
 # search for programs in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
