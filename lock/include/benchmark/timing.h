0663b902d2a0a927b9d4be75921baaf0075beafa
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-11 23:58:54 -0300

    fix: Timing class using pico-sdk api functions
    Fixing benchmark functions to use pico-sdk functions.
    The other implementation isn't work because uses platform api dependent
    functions.

--- lock/include/benchmark/timing.h
+++ lock/include/benchmark/timing.h
@@ -2,40 +2,41 @@
 #define CRYPTO_TIMING_H
 
 #include "pch.h"
+#include "hardware/timer.h"
+#include "hardware/irq.h"
 #include <iostream>
-#include <chrono>
 
 namespace crypto::benchmark {
 
 class Timer {
    private:
-    std::chrono::time_point<std::chrono::high_resolution_clock>
-        m_start_time_point;
+    static absolute_time_t m_start_time_point;
 
    public:
     Timer() {
-        m_start_time_point = std::chrono::high_resolution_clock::now();
+        m_start_time_point = get_absolute_time();
     }
     ~Timer() { stop(); }
 
     void stop() {
-        auto end_time_point = std::chrono::high_resolution_clock::now();
-
-        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(
-                         m_start_time_point)
-                         .time_since_epoch()
-                         .count();
-        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(
-                       end_time_point)
-                       .time_since_epoch()
-                       .count();
-
-        auto duration = end - start;
+        int64_t duration;
+        auto end_time_point = get_absolute_time();
+        duration = absolute_time_diff_us(m_start_time_point, end_time_point);
         double ms = duration * 0.001;
-
-        std::cout << "Time Elapsed: " << duration << "us (" << ms << "ms)\n";
+        std::cout << "Time elapsed: " << duration << "us (" << ms << "ms)\n";
     }
 };
+
+/// \tag::get_time[]
+// Simplest form of getting 64 bit time from the timer.
+// It isn't safe when called from 2 cores because of the latching
+// so isn't implemented this way in the sdk
+static uint64_t get_time(void) {
+    // Reading low latches the high value
+    uint32_t lo = timer_hw->timelr;
+    uint32_t hi = timer_hw->timehr;
+    return ((uint64_t) hi << 32u) | lo;
+}
 
 }
 
