7c026a4e86bd9ce1b53320c034443981ee5a3332
Lincoln Wallace <lincolnble@hotmail.com>
2022-12-15 19:55:48 -0300

    feat: Inserting average calculus

--- lock/include/benchmark/timing.h
+++ lock/include/benchmark/timing.h
@@ -8,28 +8,55 @@
 
 namespace crypto::benchmark {
 
-class Timer {
-   private:
-    absolute_time_t m_start_time_point;
+    class Timer {
+        public:
 
-   public:
-    void start(){
-        m_start_time_point = get_absolute_time();
-    }
-    Timer() {
-        m_start_time_point = get_absolute_time();
-    }
-    ~Timer() {stop();}
+        enum class type{
+            encryption,
+            decryption
+        };
 
-    void stop() {
-        int64_t duration;
-        auto end_time_point = get_absolute_time();
-        duration = absolute_time_diff_us(m_start_time_point, end_time_point);
-        double ms = duration * 0.001;
-        std::cout << "Time elapsed: " << duration << "us (" << ms << "ms)\n";
-    }
-};
+        type criptograpy_operation;
+        static uint32_t loop_counts;
+        static double decrypt_time_average;
+        static double encrypt_time_average;
 
+        
+        private:
+        absolute_time_t m_start_time_point;
+
+        public:
+        void start() {
+            m_start_time_point = get_absolute_time();
+        }
+        Timer() {
+            m_start_time_point = get_absolute_time();
+        }
+        Timer(type operation) : criptograpy_operation(operation){
+            loop_counts++;
+            m_start_time_point = get_absolute_time();
+        }
+
+        
+        ~Timer() { stop(); }
+
+        void stop() {
+            auto end_time_point = get_absolute_time();
+            int64_t duration = absolute_time_diff_us(m_start_time_point, end_time_point);
+            double ms = duration * 0.001;
+            std::cout << "Time elapsed: " << duration << "us (" << ms << "ms)\n";
+
+            if(criptograpy_operation == Timer::type::encryption){
+                encrypt_time_average = (encrypt_time_average + (double)duration)/loop_counts;
+                double ms = encrypt_time_average * 0.001;
+                std::cout << "Encryption average time: " << encrypt_time_average << "us (" << ms << "ms)\n";
+            }else{
+                decrypt_time_average = (decrypt_time_average + (double)duration)/loop_counts;
+                double ms = decrypt_time_average * 0.001;
+                std::cout << "Decryption average time: " << decrypt_time_average << "us (" << ms << "ms)\n";
+            }
+        }
+    };
 }
 
 #endif
---
+++ lock/src/benchmark/timing.cpp
@@ -0,0 +1,6 @@
+#include "benchmark/timing.h"
+
+uint32_t crypto::benchmark::Timer::loop_counts=0;
+double crypto::benchmark::Timer::decrypt_time_average=0;
+double crypto::benchmark::Timer::encrypt_time_average=0;
+
